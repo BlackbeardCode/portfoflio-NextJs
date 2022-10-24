37e54238412612304e5e0ff203fb12159677fd96
BlackbeardCode <blackbeardcode@gmail.com>
2022-10-24 18:23:25 +0200

    bbc

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,22 +5,52 @@ import {
   SectionText,
   SectionTitle,
 } from "../../styles/GlobalComponents";
+import Typical from "react-typical";
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
+import { motion } from "framer-motion";
 
 const Hero = () => (
   <Section row nopadding>
     <LeftSection>
-      <SectionTitle main center>
-        Hello there! <br />
-        Looking for a treasure?
-      </SectionTitle>
-      <SectionText>
-        Learn more about BlackBeardCode developer skills
-      </SectionText>
-      <Button onclick={() => (window.location = "https://google.com")}>
-        Learn More
-      </Button>
+      <motion.div
+        animate={{ x: 0, opacity: 1, filter: "blur(0)" }}
+        initial={{ x: -300, opacity: 0, filter: "blur(1px)" }}
+        transition={{ duration: 1 }}
+      >
+        <SectionTitle main center>
+          Hello there! <br />
+          <Typical
+            steps={[
+              "Developer",
+              1000,
+              "Designer",
+              1000,
+              "Student",
+              1000,
+              "Open Sourcer",
+            ]}
+            loop={Infinity}
+            wrapper="p"
+          />
+        </SectionTitle>
+        <SectionText>
+          <motion.p
+            animate={{ x: 0 }}
+            initial={{ x: -300 }}
+            transition={{ duration: 1 }}
+          >
+            I created this website to archive all the technologies explored in
+            my journey to become a Full Stack Developer. If you like to learn
+            how to develop amazing website and web applications or just take a
+            look at the various projects I created, feel free to browse this
+            website!
+          </motion.p>
+        </SectionText>
+        <Button onclick={() => (window.location = "https://google.com")}>
+          Learn More
+        </Button>
+      </motion.div>
     </LeftSection>
   </Section>
 );
