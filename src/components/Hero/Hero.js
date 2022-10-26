585446503f997651eaacfe785441ee93d8b0f558
BlackbeardCode <blackbeardcode@gmail.com>
2022-10-27 01:10:22 +0200

    bbc

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,54 +2,77 @@ import React from "react";
 
 import {
   Section,
+  SectionDivider,
   SectionText,
   SectionTitle,
 } from "../../styles/GlobalComponents";
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
-import { motion } from "framer-motion";
 import Typed from "react-typed";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useAnimation } from "framer-motion";
 
-const Hero = () => (
-  <Section row nopadding>
-    <LeftSection>
-      <motion.div
-        animate={{ x: 0, opacity: 1, filter: "blur(0)" }}
-        initial={{ x: -300, opacity: 0, filter: "blur(1px)" }}
-        transition={{ duration: 1 }}
-      >
-        <SectionTitle main center>
-          Hello there! <br />
-        </SectionTitle>
-        <SectionText>
-          <motion.p
-            style={{
-              minHeight: "18vh",
-              minWidth: "30vw",
-              maxWidth: "85vw",
-              marginRight: "20vw",
-            }}
-            animate={{ x: 0 }}
-            initial={{ x: -300 }}
-            transition={{ duration: 1 }}
-          >
-            <Typed
-              strings={[
-                "I created this website to archive all the technologies explored in my journey on the road to become a Full Stack Developer.",
-                "If you like to learn how to develop amazing website and web applications or just take a look at the various projects I created, feel free to browse this website!",
-              ]}
-              typeSpeed={25}
-              backSpeed={0}
-              loop
-            />
-          </motion.p>
-        </SectionText>
-        <Button onclick={() => (window.location = "https://google.com")}>
-          Learn More
-        </Button>
-      </motion.div>
-    </LeftSection>
-  </Section>
-);
+const Hero = () => {
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        opacity: 1,
+        filter: "blur(0)",
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({ x: -400, opacity: 0, filter: "blur(0.2px)" });
+    }
+    console.log("inView: ", inView);
+  }, [inView]);
+
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <motion.div animate={animation}>
+          <SectionTitle main center>
+            Hello there! <br />
+          </SectionTitle>
+          <SectionText>
+            <motion.p
+              style={{
+                minHeight: "18vh",
+                minWidth: "30vw",
+                maxWidth: "85vw",
+                marginRight: "20vw",
+              }}
+              animate={animation}
+            >
+              <Typed
+                strings={[
+                  "I created this website to archive all the technologies explored in my journey on the road to become a Full Stack Developer.",
+                  "If you like to learn how to develop amazing website and web applications or just take a look at the various projects I created, feel free to browse this website!",
+                ]}
+                typeSpeed={25}
+                backSpeed={0}
+                loop
+              />
+            </motion.p>
+          </SectionText>
+          <Button onclick={() => (window.location = "https://google.com")}>
+            Learn More
+          </Button>
+        </motion.div>
+      </LeftSection>
+      <SectionDivider ref={ref} />
+    </Section>
+  );
+};
 
 export default Hero;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -21,90 +21,143 @@ import {
 } from "../../styles/GlobalComponents";
 import { List, ListItem, ListTitle } from "./TechnologiesStyles";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useAnimation } from "framer-motion";
 
-const Technologies = () => (
-  <Section id="tech">
-    <SectionTitle>Technologies</SectionTitle>
-    <SectionDivider colorAlt style={{ marginTop: 10 }} />
-    <SectionText>
-      I've worked with a range a technologies in the web development world. From
-      Back-end To Design
-    </SectionText>
-    <List>
-      <ListItem>
-        <picture>
-          <SiHtml5 size="5rem" />
-        </picture>
-        <ListTitle>Html5</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiCss3 size="5rem" />
-        </picture>
-        <ListTitle>Css3</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiJavascript size="5rem" />
-        </picture>
-        <ListTitle>JavaScript</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiNodeDotJs size="5rem" />
-        </picture>
-        <ListTitle>NodeJs</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiReact size="5rem" />
-        </picture>
-        <ListTitle>React</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiNextDotJs size="5rem" />
-        </picture>
-        <ListTitle>NextJs</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiTailwindcss size="5rem" />
-        </picture>
-        <ListTitle>Tailwind</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiMaterialUi size="5rem" />
-        </picture>
-        <ListTitle>MaterialUi</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiBootstrap size="5rem" />
-        </picture>
-        <ListTitle>SiBootstrap</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiGit size="5rem" />
-        </picture>
-        <ListTitle>Git</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiMysql size="5rem" />
-        </picture>
-        <ListTitle>MySql</ListTitle>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <SiMongodb size="5rem" />
-        </picture>
-        <ListTitle>MongoDB</ListTitle>
-      </ListItem>
-    </List>
-  </Section>
-);
+const Technologies = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        opacity: 1,
+        filter: "blur(0)",
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({ x: -400, opacity: 0, filter: "blur(0.2px)" });
+    }
+    console.log("inView: ", inView);
+  }, [inView]);
+
+  return (
+    <Section id="tech">
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionDivider colorAlt style={{ marginTop: 10 }} />
+      <SectionText ref={ref}>
+        I've worked with a range a technologies in the web development world.
+        From Back-end To Design
+      </SectionText>
+      <List>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiHtml5 size="7rem" />
+            </picture>
+            <ListTitle>Html5</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiCss3 size="7rem" />
+            </picture>
+            <ListTitle>Css3</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiJavascript size="7rem" />
+            </picture>
+            <ListTitle>JavaScript</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiNodeDotJs size="7rem" />
+            </picture>
+            <ListTitle>NodeJs</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiReact size="7rem" />
+            </picture>
+            <ListTitle>React</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiNextDotJs size="7rem" />
+            </picture>
+            <ListTitle>NextJs</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiTailwindcss size="7rem" />
+            </picture>
+            <ListTitle>Tailwind</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiMaterialUi size="7rem" />
+            </picture>
+            <ListTitle>MaterialUi</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiBootstrap size="7rem" />
+            </picture>
+            <ListTitle>SiBootstrap</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiGit size="7rem" />
+            </picture>
+            <ListTitle>Git</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiMysql size="7rem" />
+            </picture>
+            <ListTitle>MySql</ListTitle>
+          </ListItem>
+        </motion.div>
+        <motion.div animate={animation}>
+          <ListItem>
+            <picture>
+              <SiMongodb size="7rem" />
+            </picture>
+            <ListTitle>MongoDB</ListTitle>
+          </ListItem>
+        </motion.div>
+      </List>
+    </Section>
+  );
+};
 
 export default Technologies;
